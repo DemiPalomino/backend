6b081ae691ce5318feac3fe7e1640706659691e7
DemiPalomino <168929856+DemiPalomino@users.noreply.github.com>
2025-11-22 16:44:11 -0500

    Cambios 12

--- src/modules/asistencias/asistencia.routes.js
+++ src/modules/asistencias/asistencia.routes.js
@@ -7,13 +7,12 @@ import {
   deleteAsistencia,
   registrarAsistenciaFacial
 } from "./asistencia.controller.js";
-import { verifyToken } from "../../middlewares/auth.middleware.js";
 const router = Router();
-router.get("/asistencias", verifyToken, getAsistencias);
-router.get("/asistencias/:id", verifyToken, getAsistencia);
-router.post("/asistencias", verifyToken, createAsistencia);
-router.put("/asistencias/:id", verifyToken,  updateAsistencia);
-router.delete("/asistencias/:id", verifyToken, deleteAsistencia);
-router.post("/asistencias/facial", verifyToken, registrarAsistenciaFacial);
+router.get("/asistencias",  getAsistencias);
+router.get("/asistencias/:id", getAsistencia);
+router.post("/asistencias", createAsistencia);
+router.put("/asistencias/:id", updateAsistencia);
+router.delete("/asistencias/:id", deleteAsistencia);
+router.post("/asistencias/facial", registrarAsistenciaFacial);
 
 export default router;
